16a7bae1ff91ae57415ae5a1025754b2f49670ae
Bagus Nuryasin <34181070+szczynk@users.noreply.github.com>
2023-11-08 22:06:24 +0700

    chore: penjelasan absoluteRange.js

--- src/utils/absoluteRange.js
+++ src/utils/absoluteRange.js
@@ -1,7 +1,14 @@
 export default function absoluteRange(amounts, minOrder, maxOrder) {
-  let valueAsNumber = parseInt(amounts, 10);
+  // ubah `amounts` jadi angka
+  let valueAsNumber = Number(amounts);
+  // jika bukan angka, maka return nilai minimal
   if (isNaN(valueAsNumber)) {
     return minOrder;
   }
+
+  // jika angka, maka lihat apakah angka itu berada di antara nilai minimal dan maksimal
+  // jika lebih kecil dari nilai minimal maka kembalikan nilai minimal
+  // jika lebih besar dari nilai maksimal maka kembalikan nilai maksimal
+  // jika berada di antara kedua nilai minimal dan maksimal, kembalikan angka tersebut
   return Math.min(Math.max(valueAsNumber, minOrder), maxOrder);
 }
